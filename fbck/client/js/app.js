0d7e073ee1460da994a7a00208676c43b0e6deef
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2024-04-04 06:30:12 +0100

    Delete fbck directory

--- fbck/client/js/app.js
+++
@@ -1,8 +0,0 @@
-const toggleMenu = () => {
-    let designState = document.getElementById("navitem").classList.contains("show");
-    if (designState) {
-        document.getElementById("navitem").classList.remove("show")
-    } else {
-        document.getElementById("navitem").classList.add("show")   
-    }
-}
