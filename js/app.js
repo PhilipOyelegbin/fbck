2de3943ee48b15fa46f2c290e06a200c55cd30b1
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-04-08 21:05:39 +0100

    update: gallery page updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
+// menu toggle function
 const toggleMenu = () => {
-    let designState = document.getElementById("navitem").classList.contains("show");
-    if (designState) {
-        document.getElementById("navitem").classList.remove("show")
-    } else {
-        document.getElementById("navitem").classList.add("show")   
-    }
+  let designState = document.getElementById("navitem").classList.contains("show");
+  if (designState) {
+    document.getElementById("navitem").classList.remove("show")
+  } else {
+    document.getElementById("navitem").classList.add("show")   
+  }
 }
+// end menu
+
+// Append an image to the modal
+var modal = document.getElementById('myModal');
+var images = document.querySelectorAll('#imageGallery img'); // Use querySelectorAll to get a NodeList
+var modalImg = document.getElementById("img01");
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+
+images.forEach(function(img) {
+  img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  }
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+// end modal
+
