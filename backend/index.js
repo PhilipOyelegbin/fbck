7ee776c82cbd23b609a4e9657595a3c517b57585
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-02-17 11:05:03 +0100

    feat: ci-cd for GKE deployment added

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ const corsOptions = {
   origin: [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "http://localhost:3000",
     "https://fbckosofe.com",
     "https://fbck.vercel.app",
   ],
